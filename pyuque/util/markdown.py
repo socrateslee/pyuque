0325071560cb785760c56863b52d9b494d60e4d1
socrateslee <socrateslee@users.noreply.github.com>
2019-12-14 10:15:32 +0800

    allow to_str to convert value to string

--- pyuque/util/markdown.py
+++ pyuque/util/markdown.py
@@ -3,19 +3,22 @@ Utilities for generating markdown documents.
 '''
 import re
 
-def get_markdown_table(data, cols=None):
+
+def get_markdown_table(data, cols=None, to_str=None):
+    if to_str is None:
+        to_str = str
     generate_line = lambda l: '| %s |' % (' | '.join(l))
     ret = []
     if not data:
         return ''
     if cols is None:
         cols = [''] * len(data[0])
-    ret.append(generate_line(cols))
+    ret.append(generate_line(map(to_str, cols)))
     ret.append(generate_line(['---'] * len(data[0])))
     for line in data:
         if isinstance(line, dict):
             line = [line.get(k, '') for k in cols]
-        ret.append(generate_line(line))
+        ret.append(generate_line(map(to_str, line)))
     return '\n'.join(ret)
 
 
